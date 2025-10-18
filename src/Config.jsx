0508c900ce78e2d1b011a3cfbfbc25db39d27e7e
Suresh <seyoneprojects@gmail.com>
2025-10-18 12:44:01 +0530

    Update Config.jsx

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,17 +1,17 @@
 // src/config.js
 const applicationMode = "development";
 var appName = "Playful Pencil";
-var apiUrl = "https://playfulpencil.in/api/";
+var apiUrl = "https://api.playfulpencil.in/api/";
 var pageSize = 10;
 var logo = "/logo.png";
-var imageBasePath = "https://playfulpencil.in/uploads";
+var imageBasePath = "https://api.playfulpencil.in/uploads";
 
 if (applicationMode === 'development') {
   appName = "Playful Pencil";
   
   //local
-  apiUrl = "https://playfulpencil.in/api/"; 
-  imageBasePath = "https://playfulpencil.in/uploads";  
+  apiUrl = "https://api.playfulpencil.in/api/"; 
+  imageBasePath = "https://api.playfulpencil.in/uploads";  
 
   //server
   //apiUrl = "http://13.53.39.107/api/";
